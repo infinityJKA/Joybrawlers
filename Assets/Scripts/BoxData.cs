5f536a3352f289fe31b71c15564e19425948741c
Justout <jkajustin@gmail.com>
2024-05-17 03:19:03 -0500

    solution? gives a million errors but doesn't seem to affect gameplay???

--- Assets/Scripts/BoxData.cs
+++ Assets/Scripts/BoxData.cs
@@ -4,6 +4,9 @@ using UnityEngine.Timeline;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.EventSystems;
+using System.Linq;
+using System;
+using UnityEditor.VersionControl;
 
 
 public class BoxData : MonoBehaviour
@@ -11,17 +14,43 @@ public class BoxData : MonoBehaviour
     public PlayableDirector frameData;
     public AnimationClip modelAnim;
     public int PlayerNumber; // either 1 for p1 or 2 for p2
+    public int MaximumTracks;
 
     public void StartAnim(GameObject model){
         // adds the relevant model to the timeline and the animation to play
         TimelineAsset asset = frameData.playableAsset as TimelineAsset;
+        // EraseExcessLayers(asset);
+
+        if(asset.outputTrackCount > MaximumTracks){
+            EraseOneExcessLayer(asset);
+        }
         AnimationTrack newTrack = asset.CreateTrack<AnimationTrack>("modelAnim");
         TimelineClip clip = newTrack.CreateClip(modelAnim);
         frameData.SetGenericBinding(newTrack, model);
 
-
         frameData.Play();
         Debug.Log("Play!");
     }
-    
+
+    public void EraseExcessLayers(TimelineAsset asset){
+        foreach(TrackAsset t in asset.GetOutputTracks()){ // this causes a million errors that don't seem to affect gameplay but without it the game lags like hell
+             String s = t.ToString();
+            if(s.Contains("modelAnim")){
+                asset.DeleteTrack(t);
+           }
+        }
+        Debug.Log(asset.outputTrackCount);
+    }
+
+    public void EraseOneExcessLayer(TimelineAsset asset){
+        foreach(TrackAsset t in asset.GetOutputTracks()){ // same result as earlier
+             String s = t.ToString();
+            if(s.Contains("modelAnim")){
+                asset.DeleteTrack(t);
+                return;
+           }
+        }
+    }
+
 }
+
